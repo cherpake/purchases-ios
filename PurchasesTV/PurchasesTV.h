aaed8879038e5c4bf1996fbb6c1a30c1762ab878
Evgeny Cherpak <cherpake@gmail.com>
2020-02-06 06:35:30 +0200

    add support for tvOS?

---
+++ PurchasesTV/PurchasesTV.h
@@ -0,0 +1,28 @@
+//
+//  PurchasesTV.h
+//  PurchasesTV
+//
+//  Created by Evgeny Cherpak on 06/02/2020.
+//  Copyright © 2020 Purchases. All rights reserved.
+//
+
+#import <StoreKit/StoreKit.h>
+
+//! Project version number for PurchasesTV.
+FOUNDATION_EXPORT double PurchasesTVVersionNumber;
+
+//! Project version string for PurchasesTV.
+FOUNDATION_EXPORT const unsigned char PurchasesTVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PurchasesTV/PublicHeader.h>
+
+#import "RCPurchases.h"
+#import "RCPurchaserInfo.h"
+#import "RCIntroEligibility.h"
+#import "RCOfferings.h"
+#import "RCOffering.h"
+#import "RCPackage.h"
+#import "RCPurchasesErrorUtils.h"
+#import "RCPurchasesErrors.h"
+#import "RCEntitlementInfo.h"
+#import "RCEntitlementInfos.h"
